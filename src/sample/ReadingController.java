643afcdac5fa51bd029a138a573e1ee37e572011
Hben_1 <hamzaben64@gmail.com>
2025-01-02 17:06:41 +0100

    reding wajda

--- src/sample/ReadingController.java
+++ src/sample/ReadingController.java
@@ -1,18 +1,30 @@
 package sample;
 
+import Model.Livre;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.geometry.Insets;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
+import java.util.ResourceBundle;
 
-public class ReadingController {
+public class ReadingController implements Initializable {
     @FXML
     private Label UserNameAccounts;
 
@@ -42,11 +54,42 @@ public class ReadingController {
 
     @FXML
     private HBox cardLayout;
+
+    @FXML
+    private GridPane GridReading;
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        List<Livre> r = Reading();
+        int column=0;
+        int row=1;
+        try {
+            for (Livre livre : r) {
+                FXMLLoader fx = new FXMLLoader();
+                fx.setLocation(getClass().getResource("/fxmlFile/GridB.fxml"));
+                VBox A = fx.load();
+                GridBController g = fx.getController();
+                g.setData(livre);
+                if(column==6){
+                    ++row;
+                    column=0;
+                }
+                GridReading.add(A,column++,row);
+                GridPane.setMargin(A,new Insets(10));
+
+
+            }
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    //bouton
     public void btnCtegori(ActionEvent a){
 
         openFXML("categorie",btnCtegori);
     }
-
     public void btnTopBooks(ActionEvent a){
 
         openFXML("TopBooks",btnTopBooks);
@@ -78,13 +121,6 @@ public class ReadingController {
 
 
 
-
-
-
-
-
-
-
     public void openFXML(String file ,Button b){
         try{
             Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("/fxmlFile/"+file+".fxml")));
@@ -98,5 +134,25 @@ public class ReadingController {
             System.out.println(e.getMessage());
         }
 
+    }
+    private List<Livre> Reading(){
+        List<Livre> ls = new ArrayList<>();
+        DbConnection conow = new DbConnection();
+        Connection conn = conow.getConnexion();
+        String sql = "SELECT * FROM livre;";
+        try(Statement stmt = conn.createStatement(); ResultSet res = stmt.executeQuery(sql)){
+            while(res.next()){
+                Livre l = new Livre();
+                l.setNom(res.getString(2));
+                l.setAuthor(res.getString(3));
+                l.setImgSrc(res.getString(4));
+                l.setRateSrc(res.getString(5));
+                ls.add(l);
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+        return ls;
     }
 }
